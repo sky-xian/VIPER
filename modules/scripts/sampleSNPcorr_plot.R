22a5ae3e4005cd6c4eb24a3d250384e91ecfe427
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-06-30 11:36:28 -0400

    fixed typo

--- modules/scripts/sampleSNPcorr_plot.R
+++ modules/scripts/sampleSNPcorr_plot.R
@@ -94,7 +94,7 @@ snpCorrMat <- read.table(snpCorrFile, header=TRUE, sep="\t", row.names=1, check.
 #form SAMPLEXXX.snp.chr6 or SAMPLEXXX.snp.[something]
 #WE need to EXTRACT out SAMPLEXXX from this string -> sampleNames
 sampleNames <- sapply(colnames(snpCorrMat),
-                      function(x) substr(x, 1, (regexpr('.snp',x)[1]-1))
+                      function(x) substr(x, 1, (regexpr('.snp',x)[1]-1)))
 colnames(snpCorrMat) <- sampleNames
 rownames(snpCorrMat) <- sampleNames
 

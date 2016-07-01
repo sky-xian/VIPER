adf6bde60bdc3e9d1da388a75fd347949f34bbb5
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-06-30 23:48:31 -0400

    working prototype of interactive html report

---
+++ html_report/js/viper.js
@@ -0,0 +1,48 @@
+/**--------------------------------------
+@author: Mahesh Vangala
+@email: vangalamaheshh@gmail.com
+@data: June, 30, 2016 
+----------------------------------------*/
+
+// vim: syntax=javascript tabstop=4 expandtab
+
+var data = [{
+		name: 'alignment',
+		url: './partials/alignment.html',
+		children: [{
+			name: 'genome',
+            url: './partials/genome_align.html'
+		},{
+			name: 'rRNA',
+            url: './partials/rRNA_align.html'
+		}]
+	},{
+		name: 'readQC',
+        url: './partials/readQC.html',
+        children: [{
+            name: 'read_distribution'
+        }]
+	},{
+		name: 'DE',
+        children: [{
+            name: 'volcano_plots'
+        }]
+	}];
+
+$(document).ready(function(){
+	$(function(){
+        var $tree = $('#m-left');
+		$tree.tree({ 
+			data: data,
+			autoOpen: true,
+			dragAndDrop: false
+		}).bind('tree.click', function(event){
+			var node = event.node;
+			var theURL = node.url;
+			if( theURL ){ $('#m-right').load(theURL); }
+		});
+        var node = $tree.tree('getNodeByName', 'alignment');
+        $tree.tree('selectNode',node); 
+        $('#m-right').load(node.url)
+	});
+});
---
+++ html_report/tools/jquery/jqtree/_entries/05_requirements.md
@@ -0,0 +1,6 @@
+---
+title: Requirements
+name: requirements
+---
+
+* [jQuery](http://jquery.com) 1.9+ or 2.0+

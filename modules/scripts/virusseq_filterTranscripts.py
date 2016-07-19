22219198b6251a06fcfa41d12df32ce1e10e4da9
Len Taing <lentaing@cfce2.dfci.harvard.edu>
2016-07-19 17:44:37 -0400

    Modifying output: 1. keep only TRANSCRIPTS that have FPKM >= 1.0; 2. print only the gtf attributes

--- modules/scripts/virusseq_filterTranscripts.py
+++ modules/scripts/virusseq_filterTranscripts.py
@@ -23,16 +23,19 @@ def main():
     for l in f:
         tmp = l.strip().split('\t')
         #ONLY look at chrM transcripts--where the viruses are placed
-        if tmp[0] == 'chrM':
+        #if tmp[0] == 'chrM':
+        if tmp[0] == 'chrM' and tmp[2] == 'transcript':
             hit = False
             for a in tmp[-1].split(';'):
                 foo = a.strip().split()
-                #CHECK to see that FPKM > 0.0
-                if foo and foo[0] == 'FPKM' and float(eval(foo[1])) > 0.0:
+                #CHECK to see that FPKM >= 1.0
+                if foo and foo[0] == 'FPKM' and float(eval(foo[1])) >= 1.0:
                     hit = True
                     break
             if hit:
-                print("\t".join(tmp))
+                #print("\t".join(tmp))
+                #JUST print the attributes--drop gene_id which is the first
+                print(";".join(tmp[-1].split(';')[1:]))
 
 if __name__=='__main__':
     main()

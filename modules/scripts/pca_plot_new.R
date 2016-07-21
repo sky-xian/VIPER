6d0b0c6d953fd4d3fad4736457c362a7d7ccc5ee
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-07-21 14:36:10 -0400

    added a better solution to prevent drop of R dataframe

--- modules/scripts/pca_plot_new.R
+++ modules/scripts/pca_plot_new.R
@@ -47,5 +47,5 @@ rpkmTable <- read.csv(rpkmFile, header=T, check.names=F,
                         row.names=1, stringsAsFactors=FALSE, dec='.')
 annot <- read.csv(metaFile, sep=",", header=T, row.names=1,
                       stringsAsFactors=FALSE, check.names=F, comment.char='#')
-annot <- as.data.frame(annot[, !grepl('comp_*', colnames(annot))])
+annot <- annot[, !grepl('comp_*', colnames(annot)), drop=F]
 pca_plot(rpkmTable, annot, pca_plot_out)

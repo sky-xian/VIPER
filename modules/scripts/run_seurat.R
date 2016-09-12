94c2ff68133f0aafb6367b65c1ed0059b43c9196
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-09-12 13:12:52 -0400

    seurat annot automation from metasheet

--- modules/scripts/run_seurat.R
+++ modules/scripts/run_seurat.R
@@ -20,29 +20,38 @@ if( is.element("Seurat", installed.packages())){
 
 options(error = function() traceback(2))
 
-runSeurat <- function( matrix_file, out_dir ){
+runSeurat <- function( matrix_file, metasheet, out_dir ) {
     sc.data <- read.csv( matrix_file, header=TRUE, row.names=1 )
     sc.data <- log( sc.data + 1 )
-    sc <- new( "seurat", raw.data=sc.data )
-    sc <- Setup( sc, project="Viper Single Cell Analysis", min.cells = 3, names.field = 1, names.delim = "_", min.genes = 1000, is.expr=1, )
-    png( paste(out_dir, "/mean_var_plot.png", sep=""), width = 8, height = 8, unit="in",res=300 )
-    sc <- MeanVarPlot( sc, y.cutoff = 2, x.low.cutoff = 1, fxn.x = expMean, fxn.y = logVarDivMean )
-    dev.off()
-    png( paste(out_dir, "/linear_PCA.png", sep=""), width = 8, height = 8, unit="in",res=300)
-    sc <- PCA( sc, do.print=FALSE )
-    PCAPlot( sc, 1, 2, pt.size = 2 )
-    dev.off()
-    
-    png( paste(out_dir, "/non_linear_tSNE_PCA.png", sep=""), width = 8, height = 8, unit="in",res=300 )
-    sc <- RunTSNE( sc, dims.use = 1:2, max_iter=2000 )
-    TSNEPlot( sc )
-    dev.off()
+    sc_obj <- new( "seurat", raw.data=sc.data )
 
-    png( paste(out_dir, "/viz_PCA.png", sep=""), width = 8, height = 8, unit="in",res=300 )
-    VizPCA(sc,1:2)
-    dev.off()
+    #------------------------------------
+    # loop through annotations
+    #------------------------------------
+    ann <- read.csv(metasheet, header=T, row.names=1, as.is=T, comment.char='#')
+    ann <- ann[,!grepl("comp_*", colnames( ann )), drop=F]
 
+    for( index in range(1,length(colnames(ann))) ) {
+        write(paste( "Processing ", colnames(ann)[index], sep=""), stderr() )
+        dir.create( file.path(out_dir,colnames(ann)[index]), showWarnings=F)
+        sc <- Setup( sc_obj, project="Viper Single Cell Analysis", min.cells = 3, names.field = index, names.delim = "_", min.genes = 1000, is.expr=1, )
+        png( paste(out_dir, "/", colnames(ann)[index], "/mean_var_plot.png", sep=""), width = 8, height = 8, unit="in",res=300 )
+        sc <- MeanVarPlot( sc, y.cutoff = 2, x.low.cutoff = 1, fxn.x = expMean, fxn.y = logVarDivMean )
+        dev.off()
+        png( paste(out_dir, "/", colnames(ann)[index], "/linear_PCA.png", sep=""), width = 8, height = 8, unit="in",res=300)
+        sc <- PCA( sc, do.print=FALSE )
+        PCAPlot( sc, 1, 2, pt.size = 2 )
+        dev.off()
     
+        png( paste(out_dir, "/", colnames(ann)[index], "/non_linear_tSNE_PCA.png", sep=""), width = 8, height = 8, unit="in",res=300 )
+        sc <- RunTSNE( sc, dims.use = 1:length(unique(ann[,index])), max_iter=2000 )
+        TSNEPlot( sc )
+        dev.off()
+
+        png( paste(out_dir, "/", colnames(ann)[index], "/viz_PCA.png", sep=""), width = 8, height = 8, unit="in",res=300 )
+        VizPCA(sc,1:length(unique(ann[,index])))
+        dev.off()
+    } 
 }
 
 parse_args <- function() {
@@ -54,4 +63,4 @@ parse_args <- function() {
 
 #args <- parse_args()
 args <- commandArgs( trailingOnly = TRUE )
-runSeurat( args[1], args[2] )
+runSeurat( args[1], args[2], args[3] )
